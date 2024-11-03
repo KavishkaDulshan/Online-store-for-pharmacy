a828077661d26750e8bd1a5856a0b20437854027
Pasindu-002 <rmpmbandara@students.nsbm.ac.lk>
2024-11-03 10:15:21 +0530

    Add files via upload

---
+++ login/login.php
@@ -0,0 +1,27 @@
+<?php
+// login.php
+
+require 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $pdo->prepare("SELECT password FROM users WHERE email = ?");
+    $stmt->execute([$username]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($user) {
+        if (password_verify($password, $user['password'])) {
+            session_start();
+            $_SESSION['username'] = $username;
+            header("Location: signup.html");
+            exit();
+        } else {
+            echo "Error: Incorrect password.";
+        }
+    } else {
+        echo "Error: No user found with that username.";
+    }
+}
+?>
