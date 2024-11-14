22b5158f7f652781c0d69f6c274eea1ddd5de568
W.K.R.P Perera <wkrpperera@students.nsbm.ac.lk>
2024-11-14 15:13:46 +0530

    db.php

--- admin panel/db.php
+++ admin panel/db.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "adminpn";
+$dbname = "pharmacy_data";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
