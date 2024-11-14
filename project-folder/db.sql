015de72828d36ab35ac43519a79480f7955c5052
PDK Dulshan <pdkdulshan@students.nsbm.ac.lk>
2024-11-14 09:57:32 +0530

    Delete project-folder directory

--- project-folder/db.sql
+++
@@ -1,9 +0,0 @@
- sql
-     CREATE TABLE Payment (
-         PaymentID INT AUTO_INCREMENT PRIMARY KEY,
-         OrderID INT,
-         Amount DECIMAL(10, 2) NOT NULL,
-         PaymentDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-         PaymentMethod VARCHAR(50),
-         FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
-     );
